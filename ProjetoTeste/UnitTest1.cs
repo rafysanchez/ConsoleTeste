fd8eab79581536e9255b6c08191abd9623efa418
rafael sanchez port <rafysanchez@hotmail.com>
2019-11-12 17:31:40 -0300

    test

---
+++ ProjetoTeste/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjetoTeste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
